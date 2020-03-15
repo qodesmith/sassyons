199117ffc2bd3c08079727189fa4cb308045068c
Aaron Cordova <theqodesmith@gmail.com>
2020-03-15 09:51:15 -0400

    Updated packages, no longer using postcss-extract-media-query.

--- compile-assets.js
+++ compile-assets.js
@@ -3,20 +3,15 @@ const fs = require('fs')
 const scss = require('node-sass')
 const postCss = require('postcss')
 const noComment = require('postcss-discard-comments')
-const extractMediaQuery = require('postcss-extract-media-query')
 const combineSelectors = require('postcss-combine-duplicated-selectors')
+const combineMediaQuery = require('postcss-combine-media-query')
 
 
 const dir = path.resolve(__dirname, 'dist')
 const postCssPlugins = [
   noComment(),
   combineSelectors({ removeDuplicatedProperties: true }),
-  extractMediaQuery({
-    combine: true, // http://bit.ly/31Oi5WK
-    output: { // http://bit.ly/2Z6BrUX - ensures it ONLY combines media queries & doesn't make multiple files.
-      path: null
-    }
-  })
+  combineMediaQuery()
 ]
 const scssOptions = {
   file: path.resolve(__dirname, 'scss/sassyons.scss'),
@@ -25,7 +20,7 @@ const scssOptions = {
 
 scss.render(scssOptions, (err1, results1) => {
   if (err1) {
-    console.error(err1)
+    console.error('ERROR 1:', err1)
     process.exit(1)
   }
 
